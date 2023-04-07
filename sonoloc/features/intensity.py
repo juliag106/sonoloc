12b4272f11509abb6990ed4a24ee6fab61439c5d
Lucy Liang <294820961+juliag106@users.noreply.github.com>
2023-04-07 14:11:40 +0800

    feat(features): active intensity vectors

---
+++ sonoloc/features/intensity.py
@@ -0,0 +1,48 @@
+"""一阶 Ambisonics（FOA）声强向量特征。
+
+FOA 四通道按 ACN/SN3D 约定排列为 ``[W, X, Y, Z]``。有源声强
+``I = Re{ W* · [X, Y, Z] }`` 指向声能流动方向，其反方向即声源方位。
+归一化后的声强向量是 SELD 中常用的空间特征。
+"""
+
+from __future__ import annotations
+
+import numpy as np
+
+
+def intensity_vectors(foa_spec: np.ndarray, eps: float = 1e-10) -> np.ndarray:
+    """由 FOA STFT 计算归一化有源声强向量。
+
+    Parameters
+    ----------
+    foa_spec:
+        形状 ``(4, n_freq, n_frames)`` 的复数 STFT，通道顺序 ``[W, X, Y, Z]``。
+
+    Returns
+    -------
+    ndarray
+        形状 ``(3, n_freq, n_frames)`` 的归一化声强特征，取值在 ``[-1, 1]``。
+    """
+    if foa_spec.shape[0] != 4:
+        raise ValueError("声强向量需要 4 通道 FOA 输入 [W, X, Y, Z]")
+    w = foa_spec[0]
+    xyz = foa_spec[1:4]
+    intensity = np.real(np.conj(w)[None, ...] * xyz)
+    denom = np.abs(w) ** 2 + np.sum(np.abs(xyz) ** 2, axis=0)
+    return intensity / (denom[None, ...] + eps)
+
+
+def intensity_doa(foa_spec: np.ndarray) -> tuple[float, float]:
+    """基于全局有源声强估计单声源的 ``(azimuth, elevation)``（弧度）。"""
+    w = foa_spec[0]
+    xyz = foa_spec[1:4]
+    intensity = np.real(np.conj(w)[None, ...] * xyz)
+    mean_i = intensity.reshape(3, -1).mean(axis=1)
+    direction = -mean_i  # 声源方向与能流方向相反
+    norm = np.linalg.norm(direction)
+    if norm < 1e-12:
+        return 0.0, 0.0
+    direction = direction / norm
+    azimuth = float(np.arctan2(direction[1], direction[0]))
+    elevation = float(np.arctan2(direction[2], np.hypot(direction[0], direction[1])))
+    return azimuth, elevation
