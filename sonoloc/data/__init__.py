e47f9d8f02f8e5c1d83b67f3f8ef00fa69f9e7f3
Lucy Liang <294820961+juliag106@users.noreply.github.com>
2023-10-20 14:51:32 +0800

    feat(data): free-field spatializer

--- sonoloc/data/__init__.py
+++ sonoloc/data/__init__.py
@@ -1,5 +1,6 @@
 """合成数据：多通道场景渲染与噪声。"""
 
 from sonoloc.data.scene import Scene, SoundEvent
+from sonoloc.data.simulate import random_scene, simulate_scene, spatialize
 
-__all__ = ["Scene", "SoundEvent"]
+__all__ = ["Scene", "SoundEvent", "random_scene", "simulate_scene", "spatialize"]
