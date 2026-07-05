701a4acb4eb20c61a398d25498899caaf676eab9
Lucy Liang <294820961+juliag106@users.noreply.github.com>
2026-07-05 15:34:10 +0800

    feat(labels): ACCDOA encode and decode

--- sonoloc/labels/__init__.py
+++ sonoloc/labels/__init__.py
@@ -1,1 +1,11 @@
 """标签工具：事件类别词表与 ACCDOA 编解码。"""
+
+from sonoloc.labels.accdoa import decode_accdoa, encode_accdoa
+from sonoloc.labels.events import DEFAULT_CLASSES, EventVocabulary
+
+__all__ = [
+    "DEFAULT_CLASSES",
+    "EventVocabulary",
+    "decode_accdoa",
+    "encode_accdoa",
+]
